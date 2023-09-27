0d93038e868ccd9d0a531835782d9cc9c95a1c77
Arifin <ar.wijaya1221@gmail.com>
2023-09-27 19:40:58 +0700

    a

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -9,48 +9,217 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="<?= base_url('assets/css/login.css') ?>" />
 </head>
+<style>
+    html {
+        height: 100%;
+    }
 
-<body style="display: flex; justify-content:center ; align-items: center;height: 100vh;overflow: hidden;">
-    <div class="form-wrap">
-        <div class="tabs">
-            <h3 class="signup-tab"><a  href="#signup-tab-content"
-                    style="border-top-left-radius: 7px;">Sign Up</a></h3>
-            <h3 class="login-tab"><a class="active" href="#login-tab-content" style="border-top-right-radius: 7px;">Login</a></h3>
-        </div>
-        <div class="tabs-content">
-            <div id="signup-tab-content">
-                <form class="signup-form" action="<?= base_url('login/register') ?>" method="post">
-                    <input type="text" class="input" id="user_email" autocomplete="off" name="nama_user"
-                        placeholder="Name">
-                    <input type="text" class="input" id="user_name" autocomplete="off" name="username"
-                        placeholder="Username">
-                    <input type="password" class="input" id="user_pass" autocomplete="off" name="password"
-                        placeholder="Password">
-                    <input type="submit" class="button" value="Sign Up">
-                </form>
-                <div class="help">
-                    <p>By signing up, you agree to our</p>
-                    <p><a href="https://www.web-leb.com/">Terms of service</a></p>
-                </div>
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+        background: linear-gradient(#30142b, #a12727);
+    }
+
+    .login-box {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 400px;
+        padding: 40px;
+        transform: translate(-50%, -50%);
+        background: rgba(0, 0, 0, .5);
+        box-sizing: border-box;
+        box-shadow: 0 15px 25px rgba(0, 0, 0, .6);
+        border-radius: 10px;
+    }
+
+    .login-box h2 {
+        margin: 0 0 30px;
+        padding: 0;
+        color: #fff;
+        text-align: center;
+    }
+
+    .login-box .user-box {
+        position: relative;
+    }
+
+    .login-box .user-box input {
+        width: 100%;
+        padding: 10px 0;
+        font-size: 16px;
+        color: #fff;
+        margin-bottom: 30px;
+        border: none;
+        border-bottom: 1px solid #fff;
+        outline: none;
+        background: transparent;
+    }
+
+    .login-box .user-box label {
+        position: absolute;
+        top: 0;
+        left: 0;
+        padding: 10px 0;
+        font-size: 16px;
+        color: #fff;
+        pointer-events: none;
+        transition: .5s;
+    }
+
+    .login-box .user-box input:focus~label,
+    .login-box .user-box input:valid~label {
+        top: -20px;
+        left: 0;
+        color: #f68e44;
+        font-size: 12px;
+    }
+
+    .login-box form button {
+        position: relative;
+        display: inline-block;
+        padding: 10px 20px;
+        color: #b79726;
+        font-size: 16px;
+        text-decoration: none;
+        text-transform: uppercase;
+        overflow: hidden;
+        transition: .5s;
+        margin-top: 40px;
+        letter-spacing: 4px;
+        background: transparent; /* Atau background: none; */
+
+    }
+
+    .login-box button:hover {
+        background: #f49803;
+        color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 5px #f4c803,
+            0 0 25px #bd9d0b,
+            0 0 50px #f4e403,
+            0 0 100px #d5cf1e;
+    }
+
+    .login-box button span {
+        position: absolute;
+        display: block;
+    }
+
+    .login-box button span:nth-child(1) {
+        top: 0;
+        left: -100%;
+        width: 100%;
+        height: 2px;
+        background: linear-gradient(90deg, transparent, #f4c003);
+        animation: btn-anim1 1s linear infinite;
+    }
+
+    @keyframes btn-anim1 {
+        0% {
+            left: -100%;
+        }
+
+        50%,
+        100% {
+            left: 100%;
+        }
+    }
+
+    .login-box button span:nth-child(2) {
+        top: -100%;
+        right: 0;
+        width: 2px;
+        height: 100%;
+        background: linear-gradient(180deg, transparent, #f4bc03);
+        animation: btn-anim2 1s linear infinite;
+        animation-delay: .25s
+    }
+
+    @keyframes btn-anim2 {
+        0% {
+            top: -100%;
+        }
+
+        50%,
+        100% {
+            top: 100%;
+        }
+    }
+
+    .login-box button span:nth-child(3) {
+        bottom: 0;
+        right: -100%;
+        width: 100%;
+        height: 2px;
+        background: linear-gradient(270deg, transparent, #f4dc03);
+        animation: btn-anim3 1s linear infinite;
+        animation-delay: .5s
+    }
+
+    @keyframes btn-anim3 {
+        0% {
+            right: -100%;
+        }
+
+        50%,
+        100% {
+            right: 100%;
+        }
+    }
+
+    .login-box button span:nth-child(4) {
+        bottom: -100%;
+        left: 0;
+        width: 2px;
+        height: 100%;
+        background: linear-gradient(360deg, transparent, #f4b003);
+        animation: btn-anim4 1s linear infinite;
+        animation-delay: .75s
+    }
+
+    @keyframes btn-anim4 {
+        0% {
+            bottom: -100%;
+        }
+
+        50%,
+        100% {
+            bottom: 100%;
+        }
+    }
+</style>
+
+
+<body>
+    <div class="login-box">
+        <h2>Login</h2>
+        <form action="<?= base_url('login/login') ?>" method="post">
+            <div class="user-box">
+                <input type="text" name="username" required="">
+                <label>Username</label>
             </div>
-            <div id="login-tab-content" class="active">
-                <form class="login-form" action="<?= base_url('login/login') ?>" method="post">
-                    <input type="text" class="input" id="user_name" autocomplete="off" name="username"
-                        placeholder="Username">
-                    <input type="password" class="input" id="user_pass" autocomplete="off" name="password"
-                        placeholder="Password">
-                    <input type="checkbox" class="checkbox" id="remember_me">
-                    <label for="remember_me">Remember me</label>
-                    <input type="submit" class="button" value="Login">
-                </form>
-                <div class="help">
-                    <p><a href="https://www.web-leb.com/">Forget your password?</a></p>
-                </div>
+            <div class="user-box">
+                <input type="password" name="password" required="">
+                <label>Password</label>
             </div>
-        </div>
+            <button type="submit">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+            </button>
+        </form>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="<?= base_url('assets/js/login.js') ?>"></script>
+</body>
+
+</html>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="<?= base_url('assets/js/login.js') ?>"></script>
 </body>
 
 </html>
